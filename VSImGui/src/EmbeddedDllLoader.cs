f8fc13b3074333328ce7a4fc977c2d8c2b85100d
maltiez2 <maltiez2@gmail.com>
2023-12-01 15:06:22 +0300

    Add project files.

---
+++ VSImGui/src/EmbeddedDllLoader.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Vintagestory;
+using Vintagestory.API.Config;
+
+namespace VSImGui
+{
+    public class EmbeddedDllClass // Author: Rasmus Tanggaard (Dmitry221060 - VS discord server) (https://github.com/Ridderrasmus)
+    {
+        private static string tempFolder;
+
+        public static void ExtractEmbeddedDlls()
+        {
+            if (RuntimeEnv.OS != OS.Windows) return;
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string[] resourceNames = assembly.GetManifestResourceNames();
+
+            foreach (var dllName in resourceNames)
+            {
+                ExtractEmbeddedDll(dllName);
+            }
+        }
+
+        public static void ExtractEmbeddedDll(string resourceName)
+        {
+            if (RuntimeEnv.OS != OS.Windows) return;
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            AssemblyName assemblyName = assembly.GetName();
+            tempFolder ??= $"{assemblyName.Name}.{assemblyName.Version}";
+
+            byte[] resourceBytes;
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                var memoryStream = new MemoryStream();
+                stream.CopyTo(memoryStream);
+                resourceBytes = memoryStream.ToArray();
+            }
+
+            string dirName = Path.Combine(Path.GetTempPath(), tempFolder);
+            if (!Directory.Exists(dirName)) Directory.CreateDirectory(dirName);
+
+            string[] resourceNameParts = resourceName.Split(".");
+            string dllName = $"{resourceNameParts[resourceNameParts.Length - 2]}.{resourceNameParts[resourceNameParts.Length - 1]}";
+            string dllPath = Path.Combine(dirName, dllName);
+            bool alreadyExtracted = false;
+            if (File.Exists(dllPath))
+            {
+                byte[] existingResource = File.ReadAllBytes(dllPath);
+                alreadyExtracted = resourceBytes.SequenceEqual(existingResource);
+            }
+            if (alreadyExtracted) return;
+            File.WriteAllBytes(dllPath, resourceBytes);
+        }
+
+        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern IntPtr LoadLibrary(string fileName);
+
+        public static bool LoadDll(string dllName)
+        {
+            if (tempFolder == null) throw new Exception("Cannot load embedded dlls before extracting them");
+
+            string dllPath = Path.Combine(Path.GetTempPath(), tempFolder, dllName);
+            IntPtr handle = LoadLibrary(dllPath);
+
+            if (handle == IntPtr.Zero)
+            {
+                Exception innerException = new Win32Exception();
+                Exception e = new DllNotFoundException("Unable to load library: " + dllName + " from " + tempFolder, innerException);
+                Console.WriteLine($"Failed to load embedded DLL:\n{e}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ VSImGui/src/VSImGuiModSystem.cs
@@ -0,0 +1,101 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.Client.NoObf;
+
+namespace VSImGui
+{
+    public class VSImGuiModSystem : ModSystem
+    {
+        public event Action SetUpImGuiWindows;
+        
+        private bool mImGuiInitialized = false;
+        private ImGuiController mController;
+        private ICoreClientAPI mApi;
+        private bool mMouseGrabbed;
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            base.StartClientSide(api);
+
+            EmbeddedDllClass.ExtractEmbeddedDlls();
+            EmbeddedDllClass.LoadDll("cimgui.dll");
+
+            mApi = api;
+
+            HarmonyPatches.Patch("vsimgui");
+            HarmonyPatches.SwapBuffersEvent += OnSwapBuffers;
+            HarmonyPatches.OnResizeEvent += OnResize;
+            HarmonyPatches.OnUpdateFrameEvent += OnUpdateFrame;
+            HarmonyPatches.OnTextInputFrameEvent += OnTextInput;
+            HarmonyPatches.OnMouseWheelEvent += OnMouseWheel;
+            HarmonyPatches.OnUpdateCameraYawPitchEvent += ResetMousePosition;
+        }
+
+        public override void Dispose()
+        {
+            if (mApi != null)
+            {
+                HarmonyPatches.SwapBuffersEvent -= OnSwapBuffers;
+                HarmonyPatches.OnResizeEvent -= OnResize;
+                HarmonyPatches.OnUpdateFrameEvent -= OnUpdateFrame;
+                HarmonyPatches.OnTextInputFrameEvent -= OnTextInput;
+                HarmonyPatches.OnMouseWheelEvent -= OnMouseWheel;
+                HarmonyPatches.OnUpdateCameraYawPitchEvent -= ResetMousePosition;
+
+                HarmonyPatches.Unpatch("vsimgui");
+            }
+
+            base.Dispose();
+        }
+
+        private void OnSwapBuffers()
+        {
+            if (!mImGuiInitialized)
+            {
+                mImGuiInitialized = InitImGui();
+                if (!mImGuiInitialized) return;
+            }
+
+            SetUpImGuiWindows?.Invoke();
+
+            mController.Render();
+            ImGuiController.CheckGLError("End of frame");
+        }
+
+        private void OnResize(NativeWindow window)
+        {
+            if (mImGuiInitialized) mController.WindowResized(window.ClientSize.X, window.ClientSize.Y);
+        }
+
+        private void OnUpdateFrame(GameWindow window, FrameEventArgs eventData)
+        {
+            if (mImGuiInitialized) mController.Update(window, (float)eventData.Time, !mMouseGrabbed);
+        }
+
+        private void OnTextInput(TextInputEventArgs eventData)
+        {
+            if (mImGuiInitialized) mController.PressChar((char)eventData.Unicode);
+        }
+
+        private void OnMouseWheel(OpenTK.Windowing.Common.MouseWheelEventArgs eventData)
+        {
+            if (mImGuiInitialized) mController.MouseScroll(eventData.Offset);
+        }
+
+        private bool InitImGui()
+        {
+            if (mApi == null) return false;
+            mController = new ImGuiController(mApi.Render.FrameWidth, mApi.Render.FrameHeight);
+
+            return true;
+        }
+
+        private void ResetMousePosition(ClientMain client)
+        {
+            mMouseGrabbed = client.MouseGrabbed;
+        }
+    }
+}
